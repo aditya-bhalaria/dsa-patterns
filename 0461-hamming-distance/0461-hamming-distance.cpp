0dac5dbf065c2e1aca4b326cc32d9f6bf8620932
Aditya Bhalaria <aditya.xb911@gmail.com>
2026-09-07 20:39:26 +0530

    Time: 0 ms (100%), Space: 7.8 MB (67.57%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int ans=x^y;
+        int count=0;
+        for(int i=0 ; i<32 ; i++){
+            if(ans & (1 << i)) count++;
+        }
+        return count;
+    }
+};
