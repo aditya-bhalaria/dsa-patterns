b823ab33837a5a4b30c29805f036d4b26af4b746
Aditya Bhalaria <aditya.xb911@gmail.com>
2026-09-07 21:01:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (96.18%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,27 +1,46 @@
-class Solution {
-public:
-    void subsetsI(int i, vector<vector<int>> &ans, vector<int> &nums, 
-                  int n, vector<int> &ds) {
+// class Solution {
+// public:
+//     void subsetsI(int i, vector<vector<int>> &ans, vector<int> &nums, 
+//                   int n, vector<int> &ds) {
         
-        if(i >= n) {
-            ans.push_back(ds);
-            return;
-        }
+//         if(i >= n) {
+//             ans.push_back(ds);
+//             return;
+//         }
 
-        ds.push_back(nums[i]);
-        subsetsI(i+1, ans, nums, n, ds);
-        ds.pop_back();
+//         ds.push_back(nums[i]);
+//         subsetsI(i+1, ans, nums, n, ds);
+//         ds.pop_back();
 
-        subsetsI(i+1, ans, nums, n, ds);
-    }
+//         subsetsI(i+1, ans, nums, n, ds);
+//     }
 
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> ds;
+//         int n = nums.size();
+
+//         subsetsI(0, ans, nums, n, ds);
+
+//         return ans;
+//     }
+
+   
+// };
+class Solution{
+public: 
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
         vector<vector<int>> ans;
-        vector<int> ds;
-        int n = nums.size();
-
-        subsetsI(0, ans, nums, n, ds);
-
-        return ans;
+        for(int i=0 ; i<(1 << n) ; i++){
+           vector<int>ds;
+           for(int j=0 ; j<n ; j++){
+            if(i & (1 << j)){
+                ds.push_back(nums[j]);
+            }
+           }
+           ans.push_back(ds);
+        }
+      return ans;
     }
 };
