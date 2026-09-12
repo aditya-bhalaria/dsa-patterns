c5fc8a8f618938ce96364a65cb22d7fd3050214b
Aditya Bhalaria <aditya.xb911@gmail.com>
2026-09-12 18:26:35 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.1%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0; i<s.length() ;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+                continue;
+            }
+            else{
+                if(st.empty()) return false;
+            }
+
+            char ch=st.top();
+            if((s[i]==')' && ch=='(') || (s[i]==']' && ch=='[') || (s[i]=='}' && ch=='{')){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+        }
+          return st.empty();
+       
+    }
+};
