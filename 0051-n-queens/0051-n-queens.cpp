541833dac4eec3af676db8f53ccbe3d9780274a5
Aditya Bhalaria <aditya.xb911@gmail.com>
2026-09-13 13:11:04 +0530

    Time: 1 ms (77.88%), Space: 10.3 MB (58.56%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    void solve(int col , vector<int> &Lr , vector<vector<string>> &ans ,vector<string> &board , vector<int> &Ud , vector<int>Ld , int n){
-    if(col == n){
-        ans.push_back(board);
-        return;
-    }
-      for(int row=0 ; row<n ; row++){
-        if(Lr[row] == 0 && Ld[row+col]==0 && Ud[(n-1) + (col-row)]==0){
-            board[row][col]='Q';
-            Lr[row]=1;
-            Ld[row+col]=1;
-            Ud[(n-1) + (col-row)]=1;
-            solve(col+1 , Lr , ans , board , Ud , Ld , n);
-            board[row][col]='.';
-            Lr[row]=0;
-            Ld[row+col]=0;
-            Ud[(n-1) + (col-row)]=0;
+    void solveNqueen(int col,vector<string>&board,vector<int>&UD,vector<int>&LD,vector<int>&LR,int n,
+    vector<vector<string>>&ans){
+        if(col == n){
+            ans.push_back(board);
+            return;
         }
-      }
 
+        for(int row=0 ; row<n ; row++){
+            if(LR[row]==0 && UD[(n-1)+(col-row)]==0 && LD[row+col]==0){
+                board[row][col]='Q';
+                LR[row]=1;
+                LD[row+col]=1;
+                UD[(n-1)+(col-row)]=1;
+                solveNqueen(col+1,board,UD,LD,LR,n,ans);
+                board[row][col]='.';
+                LR[row]=0;
+                LD[row+col]=0;
+                UD[(n-1)+(col-row)]=0;
+            }
+        }
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
-        vector<string> board(n);
+        vector<vector<string>>ans;
+        vector<string>board(n);
         string s(n,'.');
         for(int i=0 ; i<n ; i++){
             board[i]=s;
         }
-        vector<int> leftRow(n,0) , UpperD(2*n-1 , 0) , LowerD(2*n-1 , 0);
-        solve(0,leftRow,ans,board,UpperD,LowerD,n);
-        return ans;
 
+        vector<int>UpperD(2*n-1,0),LowerD(2*n-1,0),LeftRow(n,0);
+        solveNqueen(0,board,UpperD,LowerD,LeftRow,n,ans);
+        return ans;
     }
 };
