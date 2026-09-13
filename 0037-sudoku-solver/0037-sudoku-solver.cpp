2a1c424b9127fe452c15829c246c1b29822fcb66
Aditya Bhalaria <aditya.xb911@gmail.com>
2026-09-13 14:14:40 +0530

    Time: 488 ms (14.51%), Space: 8.8 MB (59%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -5,32 +5,31 @@ public:
     }
 
     bool solve(vector<vector<char>> &board){
-        for(int i=0 ; i<board.size() ; i++){
-            for(int j=0 ; j<board[0].size() ; j++){
+       for(int row=0 ; row<board.size() ; row++){
+        for(int col=0 ; col<board[0].size() ; col++){
+            if(board[row][col]=='.'){
+                for(char c='1' ; c<='9' ; c++){
+                    if(isValid(board , row , col , c)){
+                        board[row][col]=c;
+                        if(solve(board)==true) return true;
+                        else board[row][col]='.';
 
-                if(board[i][j]=='.'){
-                    for(char c='1' ; c<='9' ; c++){
-                        if(isValid(board , i , j , c)){
-                            board[i][j]=c;
-
-                            if(solve(board)==true) return true;
-                            else board[i][j]='.';
-                        }
                     }
-                return false;
                 }
+                return false;
             }
         }
-        return true;
+       }
+       return true;
     }
 
 
     bool isValid(vector<vector<char>> &board , int r , int col , char c){
-        for(int i=0 ; i<9 ; i++){
-            if(board[i][col]==c) return false;
-            if(board[r][i]==c) return false;
-            if(board[3 * (r/3) + (i/3)][3 * (col/3) + (i%3)]==c) return false;
-        }
+       for(int i=0 ; i<9 ;i++){
+         if(board[r][i]==c) return false;
+         if(board[i][col]==c) return false;
+         if(board[(3*(r/3))+(i/3)][(3*(col/3))+(i%3)]==c) return false;
+       }
         return true;
     }
 };
